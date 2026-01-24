e178b1f4a6defff88b4a8da58bf3996e42d4189c
bguerrou <boualemguerroumi21@gmail.com>
2026-01-24 21:22:18 +0100

    CPP03 ex01

---
+++ CPP/CPP03/ex01/srcs/main.cpp
@@ -0,0 +1,42 @@
+#include "../includes/ScavTrap.hpp"
+
+#define RESET   "\033[0m"
+#define GREEN   "\033[32m"
+
+int	main(void) {
+	ClapTrap	clptrp("Pirouette");
+	ScavTrap	scvtrp("Cacahuete");
+
+	std::cout << std::endl << GREEN << "- So many clappers !" << RESET << std::endl;
+	clptrp.attack("Macron");
+	scvtrp.attack("Macron");
+
+	std::cout << std::endl << GREEN << "- Macron counter attacks !" << RESET << std::endl;
+	clptrp.takeDamage(1);
+	clptrp.beRepaired(1);
+	scvtrp.takeDamage(1);
+	scvtrp.beRepaired(1);
+	std::cout << std::endl;
+
+	std::cout << GREEN << "- Oh no, " << clptrp.getName() << " has iron deficiency !" << RESET << std::endl;
+	std::cout << GREEN << "- And " << scvtrp.getName() << " too !?" << RESET << std::endl; 
+	clptrp.setEp(0);
+	scvtrp.setEp(0);
+
+	std::cout << std::endl << GREEN << "- They try to attack Adrien but..." << RESET << std::endl;
+	clptrp.attack("Adrien");
+	scvtrp.attack("Adrien");
+
+	std::cout << std::endl << GREEN << "- Even repairing thenselves..." << RESET << std::endl;
+	clptrp.beRepaired(1);
+	scvtrp.beRepaired(1);
+
+	std::cout << std::endl;
+	clptrp.takeDamage(11);
+	scvtrp.takeDamage(100);
+	std::cout << GREEN << "- CLAPTRAP, SCAVTRAP NOOOOO" << RESET << std::endl;
+	clptrp.takeDamage(1);
+	scvtrp.takeDamage(1);
+
+	return (0);
+}
