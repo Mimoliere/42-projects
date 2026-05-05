467d41ab752416c0e8d5559fd0194becaca5b01d
bguerrou <boualemguerroumi21@gmail.com>
2026-05-05 11:13:56 +0200

    CPP07 fix

--- CPP/CPP01/ex00/srcs/newZombie.cpp
+++ CPP/CPP01/ex00/srcs/newZombie.cpp
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bguerrou <bguerrou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bguerrou <boualemguerroumi21@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 14:04:21 by bguerrou          #+#    #+#             */
-/*   Updated: 2026/01/31 14:04:21 by bguerrou         ###   ########.fr       */
+/*   Updated: 2026/04/19 10:02:32 by bguerrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Zombie.hpp"
 
-Zombie*	newZombie(std::string name) {
+Zombie*		newZombie(std::string name) {
 	Zombie*	zombie = new Zombie();
 
 	zombie->setName(name);
